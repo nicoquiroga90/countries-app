fb5868ccd6b235832956444d59588d375cd8c408
Nico <akebra77@gmail.com>
2024-03-15 11:40:21 +0100

    Added more info to detail and set gitignore

--- src/componets/Detail/CountryDetail.jsx
+++ src/componets/Detail/CountryDetail.jsx
@@ -22,23 +22,38 @@ const CountryDetail = () => {
   }, [countryName]);
 
   return (
-    <div>
-      <h1>{countryName}</h1>
+    <div className="country-details">
       {countryData && (
-        <div>
-          <p>Native Name: {countryData.nativeName}</p>
-          <p>Population: {countryData.population}</p>
-          <p>Sub Region: {countryData.subregion}</p>
-          <p>Capital: {countryData.capital}</p>
-          <div>
-            <p>Top Level Domain: {countryData.topLevelDomain}</p>
-            <p>Currencies: {countryData.currencies[0].name}</p>
-           <p>Languages: {countryData.languages.map(language => language.name).join(', ')}</p>
+        <>
+          <h1>{countryData.name}</h1>
+          <div className="info-conteiner">
+            <p>Native Name: {countryData.nativeName}</p>
+            <p>Population: {countryData.population}</p>
+            <p>Sub Region: {countryData.subregion}</p>
+            <p>Capital: {countryData.capital}</p>
+            <div>
+              <p>Top Level Domain: {countryData.topLevelDomain}</p>
+              <p>Currencies: {countryData.currencies[0].name}</p>
+              <p>Languages: {countryData.languages.map(language => language.name).join(', ')}</p>
+            </div>
+          <p>Border Countries: 
+  {countryData.borders.map(border => (
+    <span key={`${border.alpha3Code}-${border.name}`} className="border-country">{getFullName(border.alpha3Code, countryData)}</span>
+  ))}
+</p>
+
+
           </div>
-        </div>
+        </>
       )}
     </div>
   );
 };
+
+const getFullName = (borderCode, countryData) => {
+  const borderCountry = countryData.borders.find(border => border.alpha3Code === borderCode);
+  return borderCountry ? borderCountry.name : 'Unknown';
+};
+
 
 export default CountryDetail;
