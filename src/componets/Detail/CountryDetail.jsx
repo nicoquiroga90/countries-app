8f331ab0297b4b29d8361609afe156cb1ce99d02
Nico <akebra77@gmail.com>
2024-03-15 12:01:19 +0100

    Border Countries fetching Names OK

--- src/componets/Detail/CountryDetail.jsx
+++ src/componets/Detail/CountryDetail.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 const CountryDetail = () => {
   const { countryName } = useParams();
   const [countryData, setCountryData] = useState(null);
+  const [allCountries, setAllCountries] = useState([]);
 
   useEffect(() => {
     const fetchCountryData = async () => {
@@ -18,42 +19,50 @@ const CountryDetail = () => {
       }
     };
 
+    const fetchAllCountries = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:4040/api/countries`
+        );
+        const data = await response.json();
+        setAllCountries(data);
+      } catch (error) {
+        console.error("Error fetching all countries:", error);
+      }
+    };
+
     fetchCountryData();
+    fetchAllCountries();
   }, [countryName]);
+
+  const getFullName = (borderCode) => {
+    const borderCountry = allCountries.find(country => country.alpha3Code === borderCode);
+    return borderCountry ? borderCountry.name : '';
+  };
 
   return (
     <div className="country-details">
       {countryData && (
         <>
           <h1>{countryData.name}</h1>
-          <div className="info-conteiner">
+          <div className="info-container">
             <p>Native Name: {countryData.nativeName}</p>
             <p>Population: {countryData.population}</p>
             <p>Sub Region: {countryData.subregion}</p>
             <p>Capital: {countryData.capital}</p>
-            <div>
-              <p>Top Level Domain: {countryData.topLevelDomain}</p>
-              <p>Currencies: {countryData.currencies[0].name}</p>
-              <p>Languages: {countryData.languages.map(language => language.name).join(', ')}</p>
-            </div>
-          <p>Border Countries: 
-  {countryData.borders.map(border => (
-    <span key={`${border.alpha3Code}-${border.name}`} className="border-country">{getFullName(border.alpha3Code, countryData)}</span>
-  ))}
-</p>
-
-
+            <p>
+              Border Countries:
+              {countryData.borders.map((borderCode, index) => (
+                <span key={index} className="border-country">
+                  {getFullName(borderCode)}
+                </span>
+              ))}
+            </p>
           </div>
         </>
       )}
     </div>
   );
 };
-
-const getFullName = (borderCode, countryData) => {
-  const borderCountry = countryData.borders.find(border => border.alpha3Code === borderCode);
-  return borderCountry ? borderCountry.name : 'Unknown';
-};
-
 
 export default CountryDetail;
