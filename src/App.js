7a912f7bd68f03cba5bd775c29470836c519e8c8
Nico <akebra77@gmail.com>
2024-03-15 15:48:44 +0100

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
-import Details from "./pages/Details"
+import Details from "./pages/Details";
+import Header from "./componets/Header/Header";
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/countries/:countryName" element={<Details />} />
--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,15 +1,12 @@
-import Theme from "./Theme";
 import "../../styles/Header.css";
 
 function Header() {
   return (
     <div className="header-conteiner">
       <h2 className="header-text">Where in the world?</h2>
-      <div className="buttom-conteiner">
-        <Theme />
-      </div>
+      <div className="buttom-conteiner"></div>
     </div>
   );
 }
 
-export default Header
+export default Header;
